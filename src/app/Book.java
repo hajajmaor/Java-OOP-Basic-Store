bdebe8efd15167576ca4f5d5bda4b9b23e21411d
hajajmaor@gmail.com <hajajmaor@hotmail.com>
2020-01-16 16:41:08 +0200

    main 4 classes built

--- src/app/Book.java
+++ src/app/Book.java
@@ -5,18 +5,42 @@ public class Book {
     private String name;
     private int inventory;
     private int year;
+    private int available;
 
     public Book() {
         this.code = 0;
         this.name = "default";
         this.inventory = 10;
         this.year = 2020;
+        this.available = this.inventory;
     }
 
-    public Book(int code, String name, int inventory, int year) {
+    public Book(int code, String name, int inventory, int year, int available) {
         this.code = code;
         this.name = name;
         this.inventory = inventory;
         this.year = year;
+        this.available = available;
+    }
+
+    public Book(String name) {
+        this.code = 0;
+        this.name = name;
+        this.inventory = 10;
+        this.year = 2020;
+        this.available = this.inventory;
+    }
+
+    private void orderCopies(Supplier supplier, int amount) {
+        this.inventory = this.inventory + supplier.orderCopies(this, amount);
+    }
+
+    public int getAvailable() {
+        return this.available;
+    }
+
+    public String toString() {
+        return "Book - code: " + code + ", name: " + name + ", available: " + available + " out of " + inventory
+                + ", year: " + year;
     }
 }
---
+++ src/app/Supplier.java
@@ -0,0 +1,31 @@
+package app;
+
+/**
+ * Supplier
+ */
+public class Supplier {
+    private int id;
+    private String name;
+    private String phone;
+    private String address;
+
+    public Supplier(int id, String name, String phone, String address) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    private int orderCopies(Book book, int amount) {
+        return amount;
+    }
+
+    private Book orderNewBook(String name) {
+        Book book = new Book(name);
+        return book;
+    }
+
+    public String toString() {
+        return "Supplier- id: " + id + ", name: " + name + ", phone: " + phone + ", address: " + address;
+    }
+}
